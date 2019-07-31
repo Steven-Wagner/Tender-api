6b389d987a392d81cbb34d3d4a7bb4a51b9d1a67
Steven Wagner <steven.glen.wagner@gmail.com>
2019-07-30 20:58:34 -0600

    Deleted commented out code

--- src/user-info/user-info.js
+++ src/user-info/user-info.js
@@ -9,14 +9,6 @@ userInfoRouter
     .all(requireAuth)
     .get((req, res, next) => {
         const user_id = req.params.user_id;
-
-        // validate ID
-        // userInfoService.validateId(user_id, res, req.app.get('db'))
-        // .then(notValidated => {
-        //     if (notValidated) {
-        //         notValidated;
-        //     }
-        //     else {
                 userInfoService.getUserInfo(user_id, req.app.get('db'))
                 .then(userInfo => 
                     res.status(200).json({
@@ -26,8 +18,6 @@ userInfoRouter
                 .catch(error => {
                     next(error);
                 })
-            // }
-        // })
     })
 
 module.exports = userInfoRouter;
